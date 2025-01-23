f430f5f5ebe71242ae9c385140f67436f151e8de
Ayyano <107408071+Ayyano@users.noreply.github.com>
2025-01-23 22:26:20 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+from RPS_game import play, quincy, abbey, kris, mrugesh
+
+play(player, quincy, 1000, verbose=True)
+play(player, abbey, 1000, verbose=True)
+play(player, kris, 1000, verbose=True)
+play(player, mrugesh, 1000, verbose=True)
